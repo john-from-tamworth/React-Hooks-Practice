7b3b9d0502227bb49851970b3b2c0f137c8c3669
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-11-03 22:39:34 +0000

    initial commit accordion component with setState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
 
-function App() {
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end Javascript framwork',
+  },
+  {
+    title: 'What is love?',
+    content: 'baby dont hurt me, dont hurt me, no more',
+  },
+  {
+    title: 'Pizza or Chinese',
+    content: 'nah',
+  },
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
-}
-
-export default App;
+};
