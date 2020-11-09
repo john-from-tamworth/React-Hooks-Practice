0998f7b2b087bcac85580bb025eb30c07cda39a5
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-11-09 22:14:57 +0000

    search component updated added axious and wiki api end point

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('coding');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const searchWiki = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    searchWiki();
+  }, [term]);
 
   return (
     <div>
