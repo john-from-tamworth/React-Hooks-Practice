e2f8090bcecffa6f1732b0ed2093033cd336ad5d
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-11-19 20:38:20 +0000

    added routing

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
+    window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
+
   return (
     <a onClick={onClick} className={className} href={href}>
       {children}
